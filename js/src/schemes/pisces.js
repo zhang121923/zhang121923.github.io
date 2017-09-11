2aa73164eca40393b88e8b7d7cd4c2be12c7aefd
zhang121923 <zhang121923@sina.com>
2017-09-11 21:54:48 +0800

    Site updated: 2017-09-11 21:54:47

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 11 2017 21:39:36 GMT+0800 (中国标准时间)
+// build time:Mon Sep 11 2017 21:53:48 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
