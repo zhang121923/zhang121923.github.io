18f0e8f5967c45df3d6575242dad7d73de154610
kerwin <kerwin@thecover.co>
2019-06-05 17:06:03 +0800

    Site updated: 2019-06-05 17:06:01

--- js/src/lazy-bg.js
+++ js/src/lazy-bg.js
@@ -1,3 +1,3 @@
-// build time:Wed Jun 05 2019 15:54:40 GMT+0800 (中国标准时间)
+// build time:Wed Jun 05 2019 17:04:33 GMT+0800 (中国标准时间)
 (function n(){window.onload=function(){document.body.style.backgroundImage="url(/images/bg.jpg)"}})(window,document);
 //rebuild by neat 
