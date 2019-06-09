4fe0415715ca852df4ba6ed107a427c5d71ce8d5
zhang121923 <zhang121923@sina.com>
2019-06-09 17:12:40 +0800

    Site updated: 2019-06-09 17:12:39

--- js/src/lazy-bg.js
+++ js/src/lazy-bg.js
@@ -1,3 +1,3 @@
-// build time:Wed Jun 05 2019 17:25:21 GMT+0800 (中国标准时间)
+// build time:Sun Jun 09 2019 17:12:30 GMT+0800 (中国标准时间)
 (function n(){window.onload=function(){document.body.style.backgroundImage="url(/images/bg.jpg)"}})(window,document);
 //rebuild by neat 
