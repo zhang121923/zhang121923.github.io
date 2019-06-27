3c4d08ad431bf6aee9daafaba7a332332fcdbbcc
kerwin <zhang121923@sina.com>
2019-06-27 12:39:57 +0800

    Site updated: 2019-06-27 12:39:57

--- js/src/lazy-bg.js
+++ js/src/lazy-bg.js
@@ -1,3 +1,3 @@
-// build time:Mon Jun 10 2019 19:18:05 GMT+0800 (中国标准时间)
+// build time:Thu Jun 27 2019 12:39:41 GMT+0800 (中国标准时间)
 (function n(){window.onload=function(){document.body.style.backgroundImage="url(/images/bg.jpg)"}})(window,document);
 //rebuild by neat 
