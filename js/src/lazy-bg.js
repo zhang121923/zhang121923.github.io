16404a5b0359e6c22578ec6900cd86c8c5cbaebc
kerwin <zhang121923@sina.com>
2020-03-08 11:32:09 +0800

    Site updated: 2020-03-08 11:32:09

--- js/src/lazy-bg.js
+++ js/src/lazy-bg.js
@@ -1,3 +1,3 @@
-// build time:Thu Jun 27 2019 12:39:41 GMT+0800 (中国标准时间)
+// build time:Sun Mar 08 2020 11:31:57 GMT+0800 (GMT+08:00)
 (function n(){window.onload=function(){document.body.style.backgroundImage="url(/images/bg.jpg)"}})(window,document);
 //rebuild by neat 
--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Thu Jun 27 2019 12:39:42 GMT+0800 (中国标准时间)
+// build time:Sun Mar 08 2020 11:31:57 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
