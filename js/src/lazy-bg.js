57af476bbdca392bd7f50c9b4fd4196514fc6113
zhanglei <zhang121923@sina.com>
2020-04-07 20:53:58 +0800

    Site updated: 2020-04-07 20:53:58

--- js/src/lazy-bg.js
+++ js/src/lazy-bg.js
@@ -1,3 +1,3 @@
-// build time:Tue Apr 07 2020 20:50:37 GMT+0800 (GMT+08:00)
+// build time:Tue Apr 07 2020 20:51:57 GMT+0800 (GMT+08:00)
 (function n(){window.onload=function(){document.body.style.backgroundImage="url(/images/bg.jpg)"}})(window,document);
 //rebuild by neat 
