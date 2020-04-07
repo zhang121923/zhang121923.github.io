24836678030ae1240c48e6409dbcf2713727900e
kerwin <zhang121923@sina.com>
2020-04-07 20:13:40 +0800

    Site updated: 2020-04-07 20:13:39

--- js/src/lazy-bg.js
+++ js/src/lazy-bg.js
@@ -1,3 +1,3 @@
-// build time:Tue Apr 07 2020 20:10:20 GMT+0800 (GMT+08:00)
+// build time:Tue Apr 07 2020 20:13:29 GMT+0800 (GMT+08:00)
 (function n(){window.onload=function(){document.body.style.backgroundImage="url(/images/bg.jpg)"}})(window,document);
 //rebuild by neat 
